32fe28292f7da4d5667aa6f7c1bd089c68fff168
Martin <martin_leiva13@hotmail.com>
2020-04-05 17:47:11 -0300

    First commit

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -1,7 +1,14 @@
+const accessToken = process.env.GATSBY_MAPBOX_KEY;
+
 export const mapServices = [
   {
     name: 'OpenStreetMap',
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  },
+  {
+    name: 'Mapbox',
+    attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>',
+    url: `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${accessToken}`
   }
 ];
